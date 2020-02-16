30fabdfd872159ddc1b7a7d3783e3eeae5de1267
Zack Morel <morelz133@gmail.com>
2020-02-15 21:13:16 -0500

    Added News Struct and working on mapping

--- Application/main.go
+++ Application/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/xml"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -10,32 +9,34 @@ import (
 // In computer science, marshalling or marshaling is the process of transforming the memory representation of an object to a data format suitable for storage or transmission, and it is typically used when data must be moved between different parts of a computer program or from one program to another.
 
 type SitemapIndex struct {
-	Locations []Location `xml:"sitemap"` // `` IS marshling and capital Locations means it will be exported
+	Locations []string `xml:"sitemap>loc"` // `` IS marshling and capital Locations means it will be exported
+}
+
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>location"`
 }
 
 // [5]type == array => this is going to have a fixed size
 // []type == slice
 
-type Location struct {
-	Loc string `xml:"loc"`
-}
-
-func (l Location) String() string { // NOTE: If you capitalize a method/receiver on a struct, it automatically gets run when user runs the struct itself
-	return fmt.Sprintf(l.Loc) // converts an interface in to a string
-}
-
 func main() {
+	var s SitemapIndex
+	var n News
+
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml") // _ in GO means to define any var that you don't intend to use because unused vars will through an error
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 
-	var s SitemapIndex
 	xml.Unmarshal(bytes, &s)
 	// fmt.Print(s.Locations)
 
-	// fmt.Printf("Here %s some %s", "are", "variables")
-
 	for _, Location := range s.Locations {
-		fmt.Printf("\n%s", Location)
+		// fmt.Printf("\n%s", Location)
+		resp, _ := http.Get(Location) // _ in GO means to define any var that you don't intend to use because unused vars will through an error
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		// resp.Body.Close()
+		xml.Unmarshal(bytes, &n)
 	}
 }
